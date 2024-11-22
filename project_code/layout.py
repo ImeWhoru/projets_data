cab78016e4649504b8f43a84ef8180bdd6877319
3rho <64255292+ImeWhoru@users.noreply.github.com>
2024-11-22 15:19:06 +0100

    setup

---
+++ project_code/layout.py
@@ -0,0 +1,14 @@
+##########
+# Page 1 #
+##########
+# visual data set + dropdown menu etc
+
+##########
+# Page 2 #
+##########
+# Clustering  + dim reduc
+
+##########
+# Page 3 #
+##########
+# Guess who + matrix correl. (mutual info)
