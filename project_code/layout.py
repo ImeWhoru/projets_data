3b33ca56b6425993ff58bf0e8ae36b674843c053
3rho <64255292+ImeWhoru@users.noreply.github.com>
2024-11-22 15:28:43 +0100

    Update layout.py

--- project_code/layout.py
+++ project_code/layout.py
@@ -1,14 +1,14 @@
 ##########
 # Page 1 #
 ##########
-# visual data set + dropdown menu etc
+# visual data set + dropdown menu etc + image moyenne
 
 ##########
 # Page 2 #
 ##########
-# Clustering  + dim reduc
+# Clustering  + dim reduc + sélection features les plus importantes
 
 ##########
 # Page 3 #
 ##########
-# Guess who + matrix correl. (mutual info)
+# Guess who + matrix correl. (mutual info) + image moyenne avec features, chacune sélectionnée fait disparaitre les images correspondantes à cette feature pour converger vers la bonne personne
